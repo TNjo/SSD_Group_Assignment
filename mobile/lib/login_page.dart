822a9101dc2e0f2a6f011d3e8cec3c01fbaf4dea
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2023-10-08 16:18:28 +0530

    signUp , Login with firease Authentication

--- mobile/lib/login_page.dart
+++ mobile/lib/login_page.dart
@@ -1,13 +1,200 @@
+// import 'package:flutter/material.dart';
+
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   State<LoginPage> createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           // Background Image
+//           Image.asset(
+//             "assets/home_img.jpg", // Replace with your image asset path
+//             height: double.infinity,
+//             width: double.infinity,
+//             scale: 0.1,
+//             fit: BoxFit.cover,
+//           ),
+//           Container(
+//             decoration: BoxDecoration(
+//               gradient: LinearGradient(
+//                 colors: [
+//                   const Color.fromARGB(0, 90, 90,
+//                       90), // You can change the transparent color to any color you want
+//                   const Color.fromARGB(110, 0, 0, 0),
+//                   const Color.fromARGB(255, 0, 0, 0).withOpacity(
+//                       1.0), // Adjust the opacity and color as needed
+//                 ],
+//                 begin: Alignment.topRight,
+//                 end: Alignment.bottomLeft,
+//               ),
+//             ),
+//           ),
+
+//           Align(
+//             alignment: Alignment.bottomCenter,
+//             child: Container(
+//               decoration: BoxDecoration(
+//                 borderRadius: const BorderRadius.only(
+//                   topLeft:
+//                       Radius.circular(30.0), // Set the top-left corner radius
+//                   topRight:
+//                       Radius.circular(30.0), // Set the top-right corner radius
+//                 ),
+//                 gradient: LinearGradient(
+//                   colors: [
+//                     Color.fromARGB(144, 0, 0, 0), 
+//                     Color.fromARGB(158, 237, 190, 21).withOpacity(1.0),
+//                   ],
+//                   begin: Alignment.topCenter,
+//                   end: Alignment.bottomCenter,
+//                 ),
+//                 // color: const Color.fromARGB(1, 237, 189, 21)
+//                 //     .withOpacity(0.7), // Background color of the small box
+//               ),
+//               height: 330, // Adjust the height as needed
+//               width: double.infinity,
+//               child: Column(
+//                 children: [
+//                   const Padding(
+//                     padding: EdgeInsets.only(top: 30.0),
+//                     child: Text(
+//                       "Login",
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 24.0,
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                   ),
+
+//                   // Email Field
+//                   SizedBox(height: 30.0),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(
+//                             10.0), // Set the corner radius here
+//                         color: Color.fromARGB(190, 217, 217, 217),
+//                       ),
+//                       child: TextFormField(
+//                         decoration: InputDecoration(
+//                           labelText: "Email",
+//                           labelStyle: TextStyle(
+//                             color: Color.fromARGB(200, 0, 0, 0),
+//                           ),
+//                           contentPadding: EdgeInsets.symmetric(
+//                               vertical: 10.0, horizontal: 15.0),
+//                           border: InputBorder.none, // Remove the default border
+//                           filled: true,
+//                         ),
+//                         cursorColor: Color.fromARGB(211, 0, 0, 0),
+//                       ),
+//                     ),
+//                   ),
+
+//                   // Password Field
+//                   const SizedBox(height: 20.0),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(
+//                             10.0), // Set the corner radius here
+//                         color: Color.fromARGB(225, 217, 217, 217),
+//                       ),
+//                       child: TextFormField(
+//                         decoration: const InputDecoration(
+//                           labelText: "Password",
+//                           labelStyle: TextStyle(
+//                             color: Color.fromARGB(200, 0, 0, 0),
+//                           ),
+//                           contentPadding: EdgeInsets.symmetric(
+//                               vertical: 10.0, horizontal: 15.0),
+//                           border: InputBorder.none, // Remove the default border
+//                           filled: true,
+//                         ),
+//                         cursorColor: const Color.fromARGB(211, 0, 0, 0),
+//                       ),
+//                     ),
+//                   ),
+
+//                   //Login Btn
+//                   const SizedBox(height: 25.0),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       Navigator.of(context).pushNamed('/sm_navbar');
+//                     },
+//                     style: ButtonStyle(
+//                       backgroundColor: MaterialStateProperty.all<Color>(
+//                           const Color.fromARGB(100, 0, 0, 0)),
+//                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+//                         RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(20.0),
+//                         ), // Set the corner radius
+//                       ),
+//                       minimumSize: MaterialStateProperty.all<Size>(
+//                           const Size(114, 45)), // Set custom button size here
+//                     ),
+//                     child: const Text("Login"),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    try {
+      final String email = emailController.text;
+      final String password = passwordController.text;
+
+      // Sign in the user with Firebase Authentication
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // After successful login, you can add additional actions here if needed.
+      // For example, you can navigate to a different screen:
+      // Navigator.of(context).pushNamed('/sm_navbar');
+
+      // Clear the text fields after successful login
+      emailController.clear();
+      passwordController.clear();
+    } catch (e) {
+      // Handle login errors (e.g., invalid credentials)
+      print('Error during login: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,23 +229,19 @@ class _LoginPageState extends State<LoginPage> {
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: const BorderRadius.only(
-                  topLeft:
-                      Radius.circular(30.0), // Set the top-left corner radius
-                  topRight:
-                      Radius.circular(30.0), // Set the top-right corner radius
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
                 ),
                 gradient: LinearGradient(
                   colors: [
-                    Color.fromARGB(144, 0, 0, 0), 
+                    Color.fromARGB(144, 0, 0, 0),
                     Color.fromARGB(158, 237, 190, 21).withOpacity(1.0),
                   ],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                 ),
-                // color: const Color.fromARGB(1, 237, 189, 21)
-                //     .withOpacity(0.7), // Background color of the small box
               ),
-              height: 330, // Adjust the height as needed
+              height: 330,
               width: double.infinity,
               child: Column(
                 children: [
@@ -74,17 +257,18 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
 
-                  // Email Field
                   SizedBox(height: 30.0),
+
+                  // Email Field
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
                     child: Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                            10.0), // Set the corner radius here
+                        borderRadius: BorderRadius.circular(10.0),
                         color: Color.fromARGB(190, 217, 217, 217),
                       ),
                       child: TextFormField(
+                        controller: emailController,
                         decoration: InputDecoration(
                           labelText: "Email",
                           labelStyle: TextStyle(
@@ -92,7 +276,7 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                           contentPadding: EdgeInsets.symmetric(
                               vertical: 10.0, horizontal: 15.0),
-                          border: InputBorder.none, // Remove the default border
+                          border: InputBorder.none,
                           filled: true,
                         ),
                         cursorColor: Color.fromARGB(211, 0, 0, 0),
@@ -101,47 +285,45 @@ class _LoginPageState extends State<LoginPage> {
                   ),
 
                   // Password Field
-                  const SizedBox(height: 20.0),
+                  SizedBox(height: 20.0),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
                     child: Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                            10.0), // Set the corner radius here
+                        borderRadius: BorderRadius.circular(10.0),
                         color: Color.fromARGB(225, 217, 217, 217),
                       ),
                       child: TextFormField(
-                        decoration: const InputDecoration(
+                        controller: passwordController,
+                        decoration: InputDecoration(
                           labelText: "Password",
                           labelStyle: TextStyle(
                             color: Color.fromARGB(200, 0, 0, 0),
                           ),
                           contentPadding: EdgeInsets.symmetric(
                               vertical: 10.0, horizontal: 15.0),
-                          border: InputBorder.none, // Remove the default border
+                          border: InputBorder.none,
                           filled: true,
                         ),
-                        cursorColor: const Color.fromARGB(211, 0, 0, 0),
+                        cursorColor: Color.fromARGB(211, 0, 0, 0),
                       ),
                     ),
                   ),
 
-                  //Login Btn
-                  const SizedBox(height: 25.0),
+                  // Login Btn
+                  SizedBox(height: 25.0),
                   ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/sm_navbar');
-                    },
+                    onPressed: _login,
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color.fromARGB(100, 0, 0, 0)),
+                          Color.fromARGB(100, 0, 0, 0)),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20.0),
-                        ), // Set the corner radius
+                        ),
                       ),
                       minimumSize: MaterialStateProperty.all<Size>(
-                          const Size(114, 45)), // Set custom button size here
+                          const Size(114, 45)),
                     ),
                     child: const Text("Login"),
                   ),
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,23 +1,79 @@
+// import 'package:flutter/material.dart';
+// import 'package:device_preview/device_preview.dart';
+
+// import 'package:mobile/components/sp_navbar.dart';
+// import 'package:mobile/components/sm_navbar.dart';
+
+// import 'package:mobile/first_page.dart';
+// import 'package:mobile/login_page.dart';
+// import 'package:mobile/signup_page.dart';
+
+// import 'package:mobile/sm_cartpage.dart';
+// import 'package:mobile/sm_homepage.dart';
+// import 'package:mobile/sm_orderspage.dart';
+// import 'package:mobile/sm_profilepage.dart';
+
+// void main() => runApp(
+//       DevicePreview(
+//         builder: (context) => MyApp(), // Wrap your app
+//       ),
+//     );
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       locale: DevicePreview.locale(context),
+//       builder: DevicePreview.appBuilder,
+//       home: FirstPage(), // Set the initial screen to Screen1.
+//       routes: <String, WidgetBuilder>{
+//         '/login':(context) => LoginPage(),
+//         '/signup':(context) => SignupPage(),
+//         '/sm_navbar':(context) => SMNavBar(),
+//         '/sm_home':(context) => SMHomePage(),
+//         '/sm_profile':(context) => SMProfilePage(),
+//         '/sm_cart':(context) => SMCartPage(),
+//         '/sm_orders':(context) => SMOrdersPage(),
+
+//         '/sp_navbar':(context) => SPNavBar()
+//       },
+//   );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mobile/components/sp_navbar.dart';
 import 'package:mobile/components/sm_navbar.dart';
-
 import 'package:mobile/first_page.dart';
 import 'package:mobile/login_page.dart';
 import 'package:mobile/signup_page.dart';
-
 import 'package:mobile/sm_cartpage.dart';
 import 'package:mobile/sm_homepage.dart';
 import 'package:mobile/sm_orderspage.dart';
 import 'package:mobile/sm_profilepage.dart';
 
-void main() => runApp(
-      DevicePreview(
-        builder: (context) => MyApp(), // Wrap your app
-      ),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCs1O7UHs5B_ObQee7HAWZvhygcEyUblyA",
+      authDomain: "csse-53.firebaseapp.com", // Update with your authDomain
+      projectId: "csse-53", // Update with your projectId
+      storageBucket: "csse-53.appspot.com", // Update with your storageBucket
+      messagingSenderId: "627858556749", // Update with your messagingSenderId
+      appId: "1:627858556749:android:94fa0cb323c8b46f8d1904",
+    ),
+  );
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,18 +81,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      home: FirstPage(), // Set the initial screen to Screen1.
+      home: FirstPage(),
       routes: <String, WidgetBuilder>{
-        '/login':(context) => LoginPage(),
-        '/signup':(context) => SignupPage(),
-        '/sm_navbar':(context) => SMNavBar(),
-        '/sm_home':(context) => SMHomePage(),
-        '/sm_profile':(context) => SMProfilePage(),
-        '/sm_cart':(context) => SMCartPage(),
-        '/sm_orders':(context) => SMOrdersPage(),
-
-        '/sp_navbar':(context) => SPNavBar()
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
+        '/sm_navbar': (context) => SMNavBar(),
+        '/sm_home': (context) => SMHomePage(),
+        '/sm_profile': (context) => SMProfilePage(),
+        '/sm_cart': (context) => SMCartPage(),
+        '/sm_orders': (context) => SMOrdersPage(),
+        '/sp_navbar': (context) => SPNavBar(),
       },
-  );
+    );
   }
 }
