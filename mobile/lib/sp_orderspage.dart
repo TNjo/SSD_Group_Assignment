63da64d337a249a1c0ecd09d16f2e2f2b6459a94
it21271328 <99182048+it21271328@users.noreply.github.com>
2023-10-08 10:19:18 +0530

    created uis of supplier

---
+++ mobile/lib/sp_orderspage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SPOrdersPage extends StatefulWidget {
+  const SPOrdersPage({super.key});
+
+  @override
+  State<SPOrdersPage> createState() => _SPOrdersPageState();
+}
+
+class _SPOrdersPageState extends State<SPOrdersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
