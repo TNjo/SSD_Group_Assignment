47bfc11aae3e00eeb5e6790e33e0df89d94e0ea9
IT21786464 <IT21786464@my.sliit.lk>
2023-10-12 13:47:42 +0530

    firebase install

---
+++ web/renoveteryx/src/utils/fbconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCi_O1Lief7hVM3gWpOjP1kruwYJlRu6wk",
+  authDomain: "csse-53.firebaseapp.com",
+  projectId: "csse-53",
+  storageBucket: "csse-53.appspot.com",
+  messagingSenderId: "627858556749",
+  appId: "1:627858556749:web:e11819c2e6448cdc8d1904",
+  measurementId: "G-E9FZ7FVR51",
+};
