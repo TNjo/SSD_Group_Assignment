a04fd6639ca14dd9e8c1a5422d10dbf4cf7011cd
IT21786464 <IT21786464@my.sliit.lk>
2023-10-16 16:52:39 +0530

    db connection

--- web/renoveteryx/src/utils/fbconfig.js
+++ web/renoveteryx/src/utils/fbconfig.js
@@ -1,4 +1,5 @@
-export default {
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
   apiKey: "AIzaSyCi_O1Lief7hVM3gWpOjP1kruwYJlRu6wk",
   authDomain: "csse-53.firebaseapp.com",
   projectId: "csse-53",
@@ -7,3 +8,5 @@ export default {
   appId: "1:627858556749:web:e11819c2e6448cdc8d1904",
   measurementId: "G-E9FZ7FVR51",
 };
+const app = initializeApp(firebaseConfig);
+export { app };
