86283ad5007b51b406b333d7164df90b492d2cb8
IT21786464 <IT21786464@my.sliit.lk>
2023-10-24 00:39:22 +0530

    admin dashboard

--- web/renoveteryx/src/pages/AdminHome/index.js
+++ web/renoveteryx/src/pages/AdminHome/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./AdminHome.css"; // Import a CSS file for styling
 
 function AdminHome() {
   return (
-    <div>
-      <h1>this is admin home t</h1>
+    <div className="admin-home-container">
+      <div className="button-container">
+        <button className="big-button">Sites</button>
+        <button className="big-button">Site Managers</button>
+        <button className="big-button">Suppliers</button>
+        <button className="big-button">Pending Orders</button>
+        <button className="big-button">My Orders</button>
+        <button className="big-button">Raise Inquiry</button>
+      </div>
     </div>
   );
 }
